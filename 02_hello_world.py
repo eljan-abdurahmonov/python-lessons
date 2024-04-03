6ea39881cb4d4b9856642b26dc849c00c931daa1
eljan-abdurahmonov <150905988+eljan-abdurahmonov@users.noreply.github.com>
2024-04-03 06:55:19 +0500

    very easy code
    hello world nomli ikkinchi dars  codes

---
+++ 02_hello_world.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 07:02:24 2024
+
+@author: eljan
+"""
+
+print('Hello World!')
+#print(Hayrli tong!)
+print(2+4*2)
+print(19/5)
+print(2**4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
